c38247c99a78d4b0932be33fce25f3ccd3028c28
moznion <moznion@gmail.com>
2014-03-06 22:31:08 +0900

    Proto

--- lib/App/GitHubWebHook2Ikachan.pm
+++ lib/App/GitHubWebHook2Ikachan.pm
@@ -2,10 +2,127 @@ package App::GitHubWebHook2Ikachan;
 use 5.008005;
 use strict;
 use warnings;
+use JSON;
+use Log::Minimal;
+use Plack::Builder;
+use Plack::Request;
+use String::IRC;
 
 our $VERSION = "0.01";
 
+sub to_app {
+    builder {
+        enable 'AccessLog';
 
+        sub {
+            my $env = shift;
+            my $req = Plack::Request->new($env);
+
+            my $channel = $req->path_info;
+            $channel =~ s!\A/+!!;
+            unless ($channel) {
+                die "Missing channerl name"; # TODO
+            }
+            infof("Post to %s", $channel);
+
+            my $payload = $req->param('payload');
+            unless ($payload) {
+                die "Payload is nothing"; # TODO
+            }
+            my $dat = decode_json($payload);
+            infof("Payload: %s", $payload);
+
+            my $branch = _extract_branch_name($dat);
+
+            # for merge commit
+            # TODO squash!
+            my $merge_commit;
+            my $head_commit = $dat->{head_commit};
+            if ($head_commit) {
+                my $head_commit_msg = $head_commit->{message};
+                if ($head_commit_msg && $head_commit_msg =~ /\AMerge (?:pull request|branch)/) {
+                    $merge_commit = [$head_commit];
+                }
+            }
+
+            # commits
+            for my $commit (@{$merge_commit || $dat->{commits} || []}) {
+                my $name = $commit->{author}->{username}
+                    || $commit->{author}->{name}
+                    || $commit->{committer}->{username}
+                    || $commit->{committer}->{name};
+                my $msg = $commit->{message};
+                my $url = $commit->{url};
+
+                send_to_ikachan($channel, $msg, $name, $url, $branch);
+            }
+
+            if (my $comment = $dat->{comment}) {
+                # issue comment
+                # action: created
+                my $msg  = $comment->{body};
+                my $name = $comment->{user}->{login};
+                my $url  = $comment->{html_url};
+
+                send_to_ikachan($channel, $msg, $name, $url, '');
+            } elsif (my $issue = $dat->{issue} and !$dat->{comment}) {
+                # issue
+                # action: opened
+                my $msg  = $issue->{body};
+                my $name = $issue->{user}->{login};
+                my $url  = $issue->{html_url};
+
+                send_to_ikachan($channel, $msg, $name, $url, '');
+            }
+
+            return [200, ['Content-Type' => 'text/plain', 'Content-Length' => 2], ['OK']];
+        };
+    };
+}
+
+sub _extract_branch_name {
+    my ($dat) = @_;
+
+    # e.g.
+    #   ref: "refs/heads/__BRANCH_NAME__"
+    my $branch;
+    if (my $ref = $dat->{ref}) {
+        $branch = (split qr!/!, $ref)[-1];
+    }
+
+    return $branch ? $branch : '';
+}
+
+sub send_to_ikachan {
+    my ($channel, $msg, $name, $url, $branch) = @_;
+
+    $msg =~ s/\r?\n.*//g;
+
+    my $green_message = "${msg} (\@${name})";
+    if ($branch) {
+        $green_message = "[$branch] $green_message";
+    }
+
+    my $text = String::IRC->new($green_message)->green . " ${url}";
+    # my $res = $ua->post($IKACHAN_URL, [
+    #     message => $text,
+    #     channel => $channel,
+    # ]);
+
+    # TODO encode
+    infof("POST %s, %s, %s, %s, %s", $channel, $msg, $name, $url, $branch || '-');
+}
+
+sub run {
+    my $class = shift;
+
+    my %args = @_ == 1 ? %{$_[0]} : @_;
+    # if (!$args{listen} && !$args{port} && !$ENV{SERVER_STARTER_PORT}) {
+    #     $args{port} = 4907;
+    # }
+    require Plack::Loader;
+    Plack::Loader->auto(%args)->run($class->to_app);
+}
 
 1;
 __END__
